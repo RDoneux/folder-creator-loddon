a16e986a77fe17b7cdc9d0e3c93ef85b5f211594
Robert <robertdoneux@gmail.com>
2022-06-21 08:31:53 +0100

    takes jdk from parent folder not git folder

--- package-and-deploy.sh
+++ package-and-deploy.sh
@@ -14,7 +14,7 @@ cd ../../folder-creator-api/api;
 cd ../../;
 cp -r folder-creator-api/api/build/launch4j/api.exe application/backend/"api.exe";
 cp -r folder-creator-api/api/build/launch4j/lib application/backend/"lib"
-cp -r folder-creator-api/api/jdk-18.0.1.1 application/backend/"jdk-18.0.1.1";
+cp -r jdk-18.0.1.1 application/backend/"jdk-18.0.1.1";
 
 cd application;
 
